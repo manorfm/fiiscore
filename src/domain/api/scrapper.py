c51f005bb15bc7f49746a830165db4649132ab6e
Manoel medeiros <manorfm@hotmail.com>
2023-08-22 23:17:01 -0300

    fixed error when dont have fii on database to not crash the app

--- src/domain/api/scrapper.py
+++ src/domain/api/scrapper.py
@@ -1,4 +1,4 @@
-from fastapi import APIRouter
+from fastapi import APIRouter, HTTPException
 from pydantic import BaseModel
 from src.domain.api.input import FIIInput, FIIForecastInput
 from src.domain.api.output import FIIOutput, FIIForecastOutput
@@ -12,25 +12,26 @@ async def add_fii(fiiInput: FIIInput) -> FIIOutput:
     """scrap FII and persist it"""
     service = FIIService()
     fii = service.save(fiiInput.name.lower())
-    return __to_response(fii)
+    return __to_response_object(fii)
 
 @router.get("/fii/scrap", tags=["fii"], status_code=200, response_model=list[FIIOutput] | None, response_model_exclude_unset=True)
 async def get_all_fiis() -> list[FIIOutput] | None:
     """scrap load all fiis persisted and scrap the indicators from this day and ordered by score"""
     service = FIIService()
     fiis = service.get_all_fii()
-    response = [*map(lambda fii: __to_response(fii), fiis)]
-    
-    return response
+    return __to_response(fiis)
 
 @router.get("/fii/scrap/forecast", tags=["fii"], status_code=200, response_model=list[FIIForecastOutput] | None, response_model_exclude_unset=True)
 async def get_all_fiis(fiiForecast: FIIForecastInput) -> list[FIIOutput] | None:
     """scrap load all fiis persisted and scrap the indicators from this day and ordered by score"""
     service = FIIService()
     fiis = service.get_all_fii_with_forecast(fiiForecast.value)
-    response = [*map(lambda fii: __to_response(fii), fiis)]
-    
-    return response
+    return __to_response(fiis)
 
-def __to_response(fii):
+def __to_response(fiis):
+    if fiis is None:
+        raise HTTPException(status_code=409, detail="No fii registred.")
+    return [*map(lambda fii: __to_response_object(fii), fiis)]
+    
+def __to_response_object(fii):
     return { **vars(fii), "score": fii.score() }
