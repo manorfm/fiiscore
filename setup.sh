730e2e45fe17cac93c562b8d637b0361a9bacb1c
Manoel medeiros <manorfm@hotmail.com>
2023-08-22 23:19:22 -0300

    created file to setup database when start empty

---
+++ setup.sh
@@ -0,0 +1,20 @@
+PORT=8000
+curl -X POST http://127.0.0.1:$PORT/fii/scrap  -H 'Content-Type: application/json' -d '{"name": "VINO11"}'
+curl -X POST http://127.0.0.1:$PORT/fii/scrap  -H 'Content-Type: application/json' -d '{"name": "hgre11"}'
+curl -X POST http://127.0.0.1:$PORT/fii/scrap  -H 'Content-Type: application/json' -d '{"name": "lgcp11"}'
+curl -X POST http://127.0.0.1:$PORT/fii/scrap  -H 'Content-Type: application/json' -d '{"name": "hslg11"}'
+curl -X POST http://127.0.0.1:$PORT/fii/scrap  -H 'Content-Type: application/json' -d '{"name": "vilg11"}'
+curl -X POST http://127.0.0.1:$PORT/fii/scrap  -H 'Content-Type: application/json' -d '{"name": "brco11"}'
+curl -X POST http://127.0.0.1:$PORT/fii/scrap  -H 'Content-Type: application/json' -d '{"name": "xplg11"}'
+curl -X POST http://127.0.0.1:$PORT/fii/scrap  -H 'Content-Type: application/json' -d '{"name": "bbpo11"}'
+curl -X POST http://127.0.0.1:$PORT/fii/scrap  -H 'Content-Type: application/json' -d '{"name": "ggrc11"}'
+curl -X POST http://127.0.0.1:$PORT/fii/scrap  -H 'Content-Type: application/json' -d '{"name": "hsml11"}'
+curl -X POST http://127.0.0.1:$PORT/fii/scrap  -H 'Content-Type: application/json' -d '{"name": "cpts11"}'
+curl -X POST http://127.0.0.1:$PORT/fii/scrap  -H 'Content-Type: application/json' -d '{"name": "HGBS11"}'
+curl -X POST http://127.0.0.1:$PORT/fii/scrap  -H 'Content-Type: application/json' -d '{"name": "VISC11"}'
+curl -X POST http://127.0.0.1:$PORT/fii/scrap  -H 'Content-Type: application/json' -d '{"name": "XPML11"}'
+
+
+#curl -X GET http://localhost:8000/fii/scrap/forecast -H 'Content-Type: application/json' -d '{"value": 2717}'
+#curl -X POST http://127.0.0.1:8000/fii/scrap  -H 'Content-Type: application/json' -d '{"name": "VINO11"}'
+
