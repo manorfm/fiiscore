faba68fb23bed4416d2848193a264ea6a3d4a81a
Manoel medeiros <manorfm@hotmail.com>
2023-08-22 23:18:04 -0300

    create docker image from codebase

--- src/infra/db/mongo_connect.py
+++ src/infra/db/mongo_connect.py
@@ -1,6 +1,12 @@
 from pymongo import MongoClient
+import os
 
 class MongoConnection:
     def connect(self):
-        return MongoClient("mongodb://root:example@localhost:27017")
+        MONGO_URL = os.getenv('MONGO_URL')
+        if MONGO_URL is None:
+            MONGO_URL = "mongodb://root:example@localhost:27017"
+
+        print(f"connecting to {MONGO_URL}")
+        return MongoClient(MONGO_URL)
         
